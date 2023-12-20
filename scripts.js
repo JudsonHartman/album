89d195fb9ac795048d1c35d6cf7ee4a951e0ec6c
Judson Hartman <104965638+JudsonHartman@users.noreply.github.com>
2023-12-19 21:38:10 -0600

    added yrics

--- scripts.js
+++ scripts.js
@@ -1,15 +1,171 @@
 const allLyrics = {
     'heavenorhell.mp3': [
-        { time: 0, text: "Lyrics line 1 for Heaven or Hell" },
-        { time: 10, text: "Lyrics line 2 for Heaven or Hell" }
-        // ... add more lyrics as needed
+        { time: 0, text: "Yeah, yeah, uh" },
+        { time: 3, text: "Uh, yeah" },
+        { time: 7, text: "Uh, yeah, yeah" },
+        { time: 10, text: "Well, what brings you to church this evenin'?" },
+        { time: 12, text: "Fightin' love, fightin' hate or you're fightin' your demons?" },
+        { time: 16, text: "Mama tried to talk to you, wouldn't kee— keep it" },
+        { time: 19, text: "Sad story, if I had to shoot 'em bet it's Robert Horry" },
+        { time: 23, text: "I know you can get comfortable with it" },
+        { time: 26, text: "I hit the road and had to double up my digits (Woah)" },
+        { time: 30, text: "My car is push-to-start, like, can you dig it?" },
+        { time: 32, text: "Put it in the driveway, my key in your ignition" },
+        { time: 35, text: "I know it get hot as hell" },
+        { time: 38, text: "But you'll be a bad one, I wanna smoke some" },
+        { time: 42, text: "I know it get hotter, yeah" },
+        { time: 46, text: "I wanna smoke some" },
+        { time: 48, text: "Yeah" },
+        { time: 54, text: "Ooh, yeah, I wanna smoke some (I wanna smoke some)" },
+        { time: 62, text: "Take me to your house and let me poke some (Ooh)" },
+        { time: 69, text: "Type of shit I gotta focus on" },
+        { time: 71, text: "Heaven or Hell? It be a story to tell (Yeah)" },
+        { time: 75, text: "Sellin' the work, I had to push out the bales (Woah)" },
+        { time: 78, text: "Me and my niggas, we steady dodgin' the 12 (Uh-huh)" },
+        { time: 81, text: " Dodgin' the jail, don't talk on the cell (Yeah)" },
+        { time: 84, text: "Lean in my orange soda, I'm Kenan and Kel (Woah)" },
+        { time: 87, text: "And in my own corners, I trap out the mail (Uh-huh)" },
+        { time: 90, text: "Fuck what you talkin' 'bout and fuck your lil' scale" },
+        { time: 93, text: "I bet it's gon' sell, I bet it's gon' sell" },
+        { time: 95, text: "It's whatever with the don, you know I'm leadin' the pack (Uh)" },
+        { time: 99, text: "You play around, I'll bet you won't get it back (Uh)" },
+        { time: 102, text: "I hit your bitch, I guess you won't get it back, uh (Hahaha)" },
+        { time: 106, text: "I wanna smoke some (I wanna smoke some)" },
+        { time: 110, text: "Yeah" },
+        { time: 114, text: "Ooh, yeah, I wanna smoke some (Yeah, yeah, yeah)" },
+        { time: 122, text: "Take me to your house and let me poke some (Ooh)" },
+        { time: 130, text: "Type of shit I gotta focus on" }
     ],
     'euphoria.mp3': [
+        { time: 0, text: "Euphoria, I'm drownin' in euphoria" },
+        { time: 22, text: "Drownin' in euphoria, euphoria" },
+        { time: 27, text: "Sleep alone by my phone all the time" },
+        { time: 30, text: "Check my phone, already knowin' it's dry" },
+        { time: 34, text: "Fake friends and old hoes, they all bangin' my line" },
+        { time: 38, text: "Tryna get the patience, but I have no time" },
+        { time: 40.5, text: "Like a dead man walking" },
+        { time: 42, text: "Try to speed time, 'cause I know you" },
+        { time: 44, text: "Funny how they never care about the old you" },
+        { time: 48, text: "I'ma blow up, that's what I'm 'posed to" },
+        { time: 51, text: "After I blow up, act like I don't know you" },
+        { time: 55, text: "Euphoria, I'm drownin' in euphoria (Oh-oh-oh, oh)" },
+        { time: 62, text: "Drownin' in euphoria, drownin' in euphoria (Oh-oh-oh, oh)" },
+        { time: 69, text: "I pulled off, off the street (Pulled off, pulled off)" },
+        { time: 73, text: "Took two down, help me bleed (Ooh)" },
+        { time: 76.5, text: "Hate when I'm in that mode (In that mode)" },
+        { time: 80, text: "I should pull up with the" },
+        { time: 84, text: "That's too easy" },
+        { time: 85.5, text: "Say somethin' to me, but I'm not good at reachin'" },
+        { time: 88, text: "I tried that reason" },
+        { time: 92, text: "Secret lust, leave alone, then come back on this side" },
+        { time: 98, text: "We both half high" },
+        { time: 101, text: "Euphoria, tell me right now if I can get high with you" },
+        { time: 109, text: "Euphoria, ask me right now and I'll tell you all the truth" },
+        { time: 117, text: "Euphoria, I'm drownin' in euphoria (Oh-oh-oh, oh)" },
+        { time: 124, text: "Drownin' in euphoria, drownin' in euphoria (Oh-oh-oh, oh)" },
+        { time: 130, text: "Seems like I came in time (Seems like)" },
+        { time: 134, text: "But I thought I could do better, babe" },
+        { time: 136, text: "Think about you every day (Seems like)" },
+        { time: 138, text: "Just like the diamond, you shine (Seems like)" },
+        { time: 140, text: "Too many girls in other states" },
+        { time: 143, text: "I wouldn't have told you anyway" },
+        { time: 145, text: "I guess you had enough (Had enough)" },
+        { time: 147, text: "I called you four times, I guess you mad or what? (Mad or)" },
+        { time: 150, text: "And me bein' me, I guess I'm outspoken (Yeah)" },
+        { time: 154, text: "I leave all these girls around my house heartbroken" },
+        { time: 157, text: "Euphoria, tell me right now if I can get high for you" },
+        { time: 164, text: "Euphoria, ask me right now, and I'll tell is all the truth" },
+        { time: 174, text: "Euphoria, I'm drownin' in euphoria" },
+        { time: 180, text: "Drownin' in euphoria, euphoria" },
+        { time: 196, text: "Euphoria" }        
+    ],
+    'cardigan.mp3': [
+        { time: 0, text: "I'm finna stand tall in it, I'm all in it (Uh-uh)" },
+        { time: 10, text: "You be on my back like a cardigan (Cardigan)" },
+        { time: 13.5, text: "Call her again, I'm in love with your friend (Call her again)" },
+        { time: 17, text: "We be doing dirt, you hide the evidence (Evidence)" },
+        { time: 20, text: "It's evident that you ready to ride (Ready to ride)" },
+        { time: 23, text: "Hop up in my Jeep and put your pride to the side (Pride to the side)" },
+        { time: 27, text: "Go against the gang, you must be ready to hide (Ready to hide)" },
+        { time: 30, text: "Swangin' on the corner, bang my tape 'til it pop (Tape 'til it pop)" },
+        { time: 34, text: "I thought I was a goner, pledged to your honor" },
+        { time: 37, text: "Burn marijuana, these diamonds is on her" },
+        { time: 40, text: "Hotter than the sauna, I met her at Salata" },
+        { time: 44, text: "Bought lil' baby Prada, she popped it for the dollar" },
+        { time: 47, text: "Don't stick around, you should save yourself (Save yourself)" },
+        { time: 51, text: "But you can hit my phone if you need some help (Needed some help)" },
+        { time: 54, text: "Don't stick around, you should save yourself (Save yourself)" },
+        { time: 57, text: "But you can hit my phone if you need some help (Needed some help)" },
+        { time: 60, text: "I'm finna stand tall in it, I'm all in it (Uh-uh)" },
+        { time: 64, text: "You be on my back like a cardigan (Cardigan)" },
+        { time: 67, text: "Call her again, I'm in love with your friend (Call her again)" },
+        { time: 71, text: "We be doing dirt, you hide the evidence (Evidence)" },
+        { time: 74, text: "It's evident that you ready to ride (Ready to ride)" },
+        { time: 78, text: "Hop up in my Jeep and put your pride to the side (Pride to the side)" },
+        { time: 81, text: "Go against the gang, you must be ready to hide (Ready to hide)" },
+        { time: 84, text: "Swangin' on the corner, bang my tape 'til it pop (Tape 'til it pop)" },
+        { time: 87, text: "I thought I was a goner, pledged to your honor" },
+        { time: 91, text: "Burn marijuana, these diamonds is on her" },
+        { time: 94, text: "Hotter than the sauna, I met her at Salata" },
+        { time: 98, text: "Bought lil' baby Prada, she popped it for the dollar" },
+        { time: 101.5, text: "Don't stick around, you should save yourself (Save yourself)" },
+        { time: 105, text: "But you can hit my phone if you need some help (Needed some help)" },
+        { time: 108, text: "Don't stick around, you should save yourself (Save yourself)" },
+        { time: 111, text: "But you can hit my phone if you need some help (Needed some help)" },
+        { time: 114, text: "I'm finna stand tall— (Tall)" },
+        { time: 119, text: "I'm all— (All)" },
+        { time: 121, text: "You be on my back like a cardigan (Cardigan)" },
+        { time: 125, text: "We be doing dirt, you hide the evidence (Evidence)" },
+        { time: 128, text: "All— (All)" },
+        { time: 133, text: "I'm all— (All)" },
+        { time: 135.5, text: "You be on my back like a cardigan" },
+        { time: 139, text: "We be doing dirt, you hide the evidence" }
+    ],
+    'afterparty.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'wasted.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'cantfeelmylegs.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'candy.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'company.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'hadenough.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'spaceship.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'nophotos.mp3': [
+        { time: 0, text: "Lyrics line 1 for Another Song" },
+        { time: 10, text: "Lyrics line 2 for Another Song" }
+        // ... add more lyrics as needed
+    ],
+    'noidea.mp3': [
         { time: 0, text: "Lyrics line 1 for Another Song" },
         { time: 10, text: "Lyrics line 2 for Another Song" }
         // ... add more lyrics as needed
     ]
-    // ... add more songs and their lyrics
 };
 
 // Function to handle each video container
